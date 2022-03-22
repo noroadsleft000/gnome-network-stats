022470ae7743c81eba3c2c85228f5604198369d7
No roads left <noroadsleft000@gmail.com>
2022-03-22 23:45:05 +0530

    Made App and Logger singleton

--- extension.js
+++ extension.js
@@ -5,26 +5,20 @@ const Me = ExtensionUtils.getCurrentExtension();
 const { Logger } = Me.imports.utils.Logger;
 const { App } = Me.imports.App;
 const { initBrodcasters, deinitBrodcasters } = Me.imports.utils.Broadcasters;
-const Prefs = Me.imports.prefs;
 
-let logger = undefined;
-let app = undefined;
 function init() {
-    logger = new Logger;
-    logger.info(`initializing 1 ${Me.metadata.name}`);
+    Logger.info(`initializing ${Me.metadata.name}`);
 }
 
 function enable() {
-    logger.info(`enabling ${Me.metadata.name}`);
+    Logger.info(`enabling ${Me.metadata.name}`);
     initBrodcasters();
-    app = new App();
-    app.start();
+    App.instance().start();
 }
 
 function disable() {
-    logger.info(`disabling ${Me.metadata.name}`);
-    app.stop();
+    Logger.info(`disabling ${Me.metadata.name}`);
+    App.instance().stop();
     deinitBrodcasters();
-    app = undefined;
-    logger = undefined;
+    App.deleteInstance();
 }
