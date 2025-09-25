ab8653ddd2d0b94502074ebf3f8f0489ac510162
No roads left <noroadsleft000@gmail.com>
2025-09-26 00:27:04 +0530

    Updated language files

--- locale/update_lang_template.sh
+++ locale/update_lang_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find ../ -type f -name "*.js" > POTFILES.in
+find ../ -type f -name "*.ts" -not -path "*/node_modules/*" > POTFILES.in
 xgettext \
 --copyright-holder="No roads left" \
 --package-name="network-stats" \
@@ -8,6 +8,7 @@ xgettext \
 --msgid-bugs-address="noroadsleft000@gmail.com" \
 --copyright-holder="`date +%Y` No roads left" \
 --no-location \
+--language=Python \
 --from-code=UTF-8 -d network-stats -o network-stats.pot -p . --keyword=_:1 -f POTFILES.in
 
 # use bellow command to generate a laguage specific po file.
